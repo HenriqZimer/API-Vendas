19b30a8794f3e873f281c7992b048bd2245f58b6
Henrique Zimermann <henrique.zimermann@trust.group>
2023-10-31 14:29:19 -0300

    modulo produtos pronto

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -4,23 +4,29 @@ import AppError from '@shared/errors/AppErrors';
 import Product from '../typeorm/entities/Product';
 
 interface IRequest {
-  name: string;
-  price: number;
-  quantity: number;
+  name: string
+  price: number
+  quantity: number
 }
 
 class CreateProductService {
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    const productsRepository = getCustomRepository(ProductRepository);
-    const productsExists = await productsRepository.findByName(name);
+    const productsRepository = getCustomRepository(ProductRepository)
+    const productsExists = await productsRepository.findByName(name)
 
     if (productsExists) {
-      throw new AppError('There is already one product with this name');
+      throw new AppError('There is already one product with this name.');
     }
+
     const product = productsRepository.create({
       name,
       price,
       quantity,
     });
+
+    await productsRepository.save(product);
+    return product
   }
 }
+
+export default CreateProductService
