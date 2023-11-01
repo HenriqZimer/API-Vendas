7d14a9529fd1dc4edbaf14ae50051c63ffd66975
Henrique Zimermann <henrique.zimermann@trust.group>
2023-11-01 16:49:27 -0300

    falta pouco

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '7ae7d8cb043fc63efc89099f3957931f',
+    expiresIn: '1d',
+  }
+}
--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
 import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+import isAuthenticated from '@shared/http/middlewares/isAutheticated';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const usersAvatarController = new UserAvatarController();
 
-usersRouter.get('/', usersController.index);
+const upload = multer(uploadConfig);
+
+usersRouter.get('/', isAuthenticated, usersController.index);
 
 usersRouter.post(
   '/',
@@ -18,5 +25,13 @@ usersRouter.post(
   }),
   usersController.create,
 );
+
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  usersAvatarController.update
+
+)
 
 export default usersRouter;
